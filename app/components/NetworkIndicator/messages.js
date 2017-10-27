a118eac673f8910ae1e748de19f63ef96a177272
PL <3682187+PaulLaux@users.noreply.github.com>
2017-10-27 02:16:57 -0700

    Created NetworkIndicator component

---
+++ app/components/NetworkIndicator/messages.js
@@ -0,0 +1,13 @@
+/*
+ * NetworkIndicator Messages
+ *
+ * This contains all the text for the NetworkIndicator component.
+ */
+import { defineMessages } from 'react-intl';
+
+export default defineMessages({
+  header: {
+    id: 'app.components.NetworkIndicator.header',
+    defaultMessage: 'This is the NetworkIndicator component !',
+  },
+});
---
+++ app/components/NetworkIndicator/tests/index.test.js
@@ -0,0 +1,10 @@
+// import React from 'react';
+// import { shallow } from 'enzyme';
+
+// import NetworkIndicator from '../index';
+
+describe('<NetworkIndicator />', () => {
+  it('Expect to have unit tests specified', () => {
+    expect(true).toEqual(false);
+  });
+});
