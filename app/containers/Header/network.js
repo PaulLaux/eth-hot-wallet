8055360eba0f6ec7149166988d84cc0e0f493f4a
PL <3682187+PaulLaux@users.noreply.github.com>
2017-10-26 19:41:29 -0700

    add svg logo

--- app/containers/Header/network.js
+++ app/containers/Header/network.js
@@ -2,8 +2,8 @@
 const Network = {
   Offline: 'offline',
   Local_RPC: 'http://127.0.0.1:8545',
-  Main_Net: 'https://mainnet.infura.io/GjiCzFxpQAUkPtDUpKEP',
   Ropsten_Test_Net: 'https://ropsten.infura.io/GjiCzFxpQAUkPtDUpKEP',
+  Main_Net: 'https://mainnet.infura.io/GjiCzFxpQAUkPtDUpKEP',
 };
 
 module.exports = Network;
