85b9a92e710026986d6beb9b094d75e71f2eb6c1
PaulLaux <3682187+PaulLaux@users.noreply.github.com>
2017-12-04 12:56:28 +0200

    changed h1 text on main page

--- app/components/WelcomeText/index.js
+++ app/components/WelcomeText/index.js
@@ -23,7 +23,7 @@ font-weight: 400;
 function WelcomeText() {
   return (
     <div>
-      <H1>Welcome to ETH Hot Wallet <br />To begin, create or restore wallet<br /></H1>
+      <H1>Welcome to ETH Hot Wallet <br />To begin, create or restore Ethereum wallet<br /></H1>
       <H2>
         ETH Hot wallet is a zero client. Conection to Ethereum network is done via infura / local node. <br />
         Keystore is encrypted using the password, When the wallet is locked you can only view balances.<br />
