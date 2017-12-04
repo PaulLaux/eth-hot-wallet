6a7d2e7b533ab81142239aaf4a785ac58363f01e
PaulLaux <3682187+PaulLaux@users.noreply.github.com>
2017-12-04 13:36:21 +0200

    Regenerated icons

--- app/app.js
+++ app/app.js
@@ -25,20 +25,36 @@ import App from 'containers/App';
 // Load the favicon, the manifest.json file and the .htaccess file
 /* eslint-disable import/no-unresolved, import/extensions */
 import '!file-loader?name=[name].[ext]!./images/favicon.ico';
-import '!file-loader?name=[name].[ext]!./images/icon-72x72.png';
-import '!file-loader?name=[name].[ext]!./images/icon-96x96.png';
-import '!file-loader?name=[name].[ext]!./images/icon-128x128.png';
-import '!file-loader?name=[name].[ext]!./images/icon-144x144.png';
-import '!file-loader?name=[name].[ext]!./images/icon-152x152.png';
-import '!file-loader?name=[name].[ext]!./images/icon-192x192.png';
-import '!file-loader?name=[name].[ext]!./images/icon-384x384.png';
-import '!file-loader?name=[name].[ext]!./images/icon-512x512.png';
+import '!file-loader?name=[name].[ext]!./images/android-chrome-36x36.png';
+import '!file-loader?name=[name].[ext]!./images/android-chrome-48x48.png';
+import '!file-loader?name=[name].[ext]!./images/android-chrome-72x72.png';
+import '!file-loader?name=[name].[ext]!./images/android-chrome-96x96.png';
+import '!file-loader?name=[name].[ext]!./images/android-chrome-144x144.png';
+import '!file-loader?name=[name].[ext]!./images/android-chrome-192x192.png';
+import '!file-loader?name=[name].[ext]!./images/android-chrome-256x256.png';
+import '!file-loader?name=[name].[ext]!./images/android-chrome-384x384.png';
+import '!file-loader?name=[name].[ext]!./images/android-chrome-512x512.png';
 import '!file-loader?name=[name].[ext]!./manifest.json';
 import 'file-loader?name=[name].[ext]!./.htaccess';
+
+import '!file-loader?name=[name].[ext]!./images/apple-touch-icon-57x57.png';
+import '!file-loader?name=[name].[ext]!./images/apple-touch-icon-60x60.png';
+import '!file-loader?name=[name].[ext]!./images/apple-touch-icon-72x72.png';
+import '!file-loader?name=[name].[ext]!./images/apple-touch-icon-76x76.png';
+import '!file-loader?name=[name].[ext]!./images/apple-touch-icon-114x114.png';
+import '!file-loader?name=[name].[ext]!./images/apple-touch-icon-120x120.png';
+import '!file-loader?name=[name].[ext]!./images/apple-touch-icon-144x144.png';
+import '!file-loader?name=[name].[ext]!./images/apple-touch-icon-152x152.png';
+import '!file-loader?name=[name].[ext]!./images/apple-touch-icon-180x180.png';
+import '!file-loader?name=[name].[ext]!./images/favicon-32x32.png';
+import '!file-loader?name=[name].[ext]!./images/favicon-16x16.png';
+import '!file-loader?name=[name].[ext]!./images/safari-pinned-tab.svg';
+import '!file-loader?name=[name].[ext]!./images/mstile-144x144.png';
 /* eslint-enable import/no-unresolved, import/extensions */
 
 import configureStore from './store';
 
+// Will be added in the future
 // Import i18n messages
 // import { translationMessages } from './i18n';
 
