18576a4a254463476b50ed6ce385a6606465bf12
Alex Edwards <32015939+youtweakit@users.noreply.github.com>
2018-01-08 22:03:31 +0300

    Adding PoA Network Support

--- app/containers/Header/network.js
+++ app/containers/Header/network.js
@@ -1,9 +1,11 @@
-
 const Network = {
   Offline: { rpc: 'offline', tx_explorer: null },
+  'PoA Network': { rpc: 'https://core.poa.network', tx_explorer: 'https://core-explorer.poa.network/tx/' },
+  'Sokol PoA Testnet': {rpc: 'https://sokol.poa.network', tx_explorer: 'https://sokol-explorer.poa.network/tx/'},
+  'Kovan Testnet': { rpc: 'https://kovan.infura.io/DI0Ayd5pD4v0mFuuR3Zn', tx_explorer: 'https://ropsten.etherscan.io/tx/'},
+  'Ropsten Testnet': { rpc: 'https://ropsten.infura.io/DI0Ayd5pD4v0mFuuR3Zn', tx_explorer: 'https://ropsten.etherscan.io/tx/' },
   'Local RPC': { rpc: 'http://127.0.0.1:8545', tx_explorer: null },
-  'Ropsten Testnet': { rpc: 'https://ropsten.infura.io/GjiCzFxpQAUkPtDUpKEP', tx_explorer: 'https://ropsten.etherscan.io/tx/' },
-  'Main Net': { rpc: 'https://mainnet.infura.io/GjiCzFxpQAUkPtDUpKEP', tx_explorer: 'https://etherscan.io/tx/' },
+  'Main Net': { rpc: 'https://mainnet.infura.io/DI0Ayd5pD4v0mFuuR3Zn', tx_explorer: 'https://etherscan.io/tx/' },
 };
 
 module.exports = Network;
