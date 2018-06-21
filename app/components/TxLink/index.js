32840a36ceb1f78124dbab480c9726d48edaf0be
Oleg Baturov <be.topcoder@gmail.com>
2018-06-22 00:00:32 +0300

    updated tx explorer link

--- app/components/TxLink/index.js
+++ app/components/TxLink/index.js
@@ -7,6 +7,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
+// import util from 'util';
 
 const Span = styled.span`
 overflow-wrap: break-word;
@@ -15,6 +16,11 @@ overflow-wrap: break-word;
 function TxLink(props) {
   const { tx, explorer } = props;
   if (explorer) {
+    // return (
+    //   <a href={`${util.format(explorer, tx)}`} target="_blank" rel="noopener">
+    //     <Span>{tx}</Span>
+    //   </a>
+    // );
     return (
       <a href={`${explorer}${tx}`} target="_blank" rel="noopener">
         <Span>{tx}</Span>
